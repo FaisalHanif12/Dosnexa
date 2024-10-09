b79bd7bd3b8f4ad9800395deec8cb3d0f46f8061
Faisal Hanif <mehrfaisal111@example.com>
2024-10-09 16:32:30 -0700

    change heading

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -11,9 +11,7 @@ const Hero = () => {
           Our Responsibility
         </h1>
         <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
-          tenetur doloremque molestias repellat minus asperiores in aperiam
-          dolor, quaerat praesentium.
+        At Dosnexa, we are committed to transforming how you access healthcare services. Our platform is designed to alleviate the hassle associated with managing medical appointments by offering a streamlined, user-friendly system. From quick appointment scheduling to seamless communication with healthcare professionals, Dosnexa makes it easier than ever to manage your health care needs efficiently and effectively.
         </p>
       </div>
       <div className="hero-img">
